bdcdc2539ddf7fc519c6bc459cc526904734622c
David Boyd <david@coderain.co.uk>
2019-03-27 21:26:37 +0000

    Remove console statements

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -4,7 +4,6 @@ module.exports = {
 
   async postPeople(parent, args, { db }) {
 
-    console.log(`args.input.squads=${args.input.squads}`);
     const newPerson = {
       ...args.input,
       squads: [
@@ -15,15 +14,10 @@ module.exports = {
     const { insertedIds } = await db.collection('People').insert(newPerson)
     newPerson._id = insertedIds[0]
 
-    console.log(`args.input.squads2=${args.input.squads}, newPerson._id=${newPerson._id}`);
-
     await db.collection('Squads')
       .update({ _id: ObjectID(args.input.squads) }, { $push: { members: newPerson._id } })
 
     return newPerson
-
-    // return db.collection('photos')
-    //   .findOne({ _id: ObjectID(args.photoID) })
 
   }
 
